e2c2ee9badf7fbab7bfcbc4d34165da4c001878f
Eliza Kennelly <elizakennelly@gmail.com>
2020-11-12 18:48:51 -0600

    updated workout model

--- models/workout.js
+++ models/workout.js
@@ -12,9 +12,45 @@ const workoutSchema = new Schema(
                 type: {
                     type: String,
                     trim: true,
-                    required: "Enter an exercise"
+                    required: "Enter an exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter exercise duration in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number
                 }
             }
         ]
+    },
+    {
+        toJSON: {
+            virtuals: ture
+        }
     }
-)
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
