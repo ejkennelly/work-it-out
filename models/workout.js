d1d02d4db030ea5e2a320d4f0d41aa267a91d038
Eliza Kennelly <elizakennelly@gmail.com>
2020-11-12 18:33:34 -0600

    edited models folder

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise"
+                }
+            }
+        ]
+    }
+)
